a1606a172fa5cd41aef2c326e86ee2fc9257de9d
Ariuska7 <47497182+Ariuska7@users.noreply.github.com>
2020-07-04 11:12:58 +0800

    修正空格错误

--- gui/AppWindow.java
+++ gui/AppWindow.java
@@ -10,14 +10,13 @@ public class AppWindow {
        String testName="";
        IntegrationView integrationView = new IntegrationView();
        GiveTestPaper initTestPaper = new RamdomInitTestPaper(); //创建初始试卷对象
-       TestPaper testPaper= 
-       initTestPaper.getTestPaper("软件发布/题库/交通理论.xls",5);   //得到有5个题目的试卷
+       TestPaper testPaper= initTestPaper.getTestPaper("软件发布/题库/交通理论.xls",5);   //得到有5个题目的试卷
        TestPaperView testView = new TestPaperView();
        testView.setTestPaper(testPaper);        //设置试卷
        testView.setTeacher(new TeacherOne()); //设置阅卷老师
        testName = "交通法训练";
        testView.setTestName(testName);
-       testView.setTotalTime(900);//考试时间15分钟,900秒
+       testView.setTotalTime(900);//考试时间15分钟,900
        integrationView.addTestPaperView(testName,testView);
        initTestPaper = new RamdomInitTestPaper(); //创建初始试卷对象
        testPaper= initTestPaper.getTestPaper("软件发布/题库/java基础.xls",6);
