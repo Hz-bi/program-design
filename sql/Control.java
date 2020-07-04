d441e8719e71ed1ac8eba71a91e1052b4906c58e
Ariuska7 <47497182+Ariuska7@users.noreply.github.com>
2020-07-04 19:04:01 +0800

    优化数据库输出

--- sql/Control.java
+++ sql/Control.java
@@ -1,9 +1,12 @@
 /*
-  使用环境依赖:
+  使用环境依赖:MySQL8.0 Navicat Premium12.0
+  驱动依赖:mysql-connector-java-8.0.20.jar
+  Control.java第二次修改:修改部分换行符、设置null字符串为空、添加部分关键注释
 */
 package sql;
 import java.sql.*;
 public class Control {
+    //必须确保使用含有test数据库才能正常加载 用户名默认root 密码更改为个人数据库设置的密码
     static final String URL = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT";
     static final String User = "root";
     static final String Pass = "123456";
@@ -16,7 +19,7 @@ public class Control {
         }
         //执行查询
         Statement stmt=(Statement)conn.createStatement(); //实例化Statement对象
-        String sql = "SELECT * FROM traffic";
+        String sql = "SELECT * FROM traffic";//切换对应表 更改表名即可
         ResultSet rs = stmt.executeQuery(sql);
         System.out.println("数据库查询结果如下:");
         while (rs.next()){
@@ -27,7 +30,11 @@ public class Control {
             String choiceD = rs.getString("选择D");
             String answer  = rs.getString("正确答案");
             String gallery = rs.getString("图片");
-            System.out.println(subject+"\n"+choiceA+" "+choiceB+" "+choiceC+" "+choiceD+"\n正确答案:"+answer+" 图片:"+gallery);
+            if(choiceA==null)choiceA="";
+            if(choiceB==null)choiceB="";
+            if(choiceC==null)choiceC="";
+            if(choiceD==null)choiceD="";
+            System.out.println(subject+"\n"+choiceA+"\n"+choiceB+"\n"+choiceC+"\n"+choiceD+"\n正确答案:"+answer+" 图片:"+gallery+"\n");
         }
         System.out.println("数据库查询结束");
     }
