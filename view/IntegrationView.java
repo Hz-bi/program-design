d352281e3d8e21a71b7baed3efa8ef0ac3692739
禾子 <60293583+Hz-bi@users.noreply.github.com>
2020-07-05 22:00:31 +0800

    付总万岁

--- view/IntegrationView.java
+++ view/IntegrationView.java
@@ -8,7 +8,9 @@ public class IntegrationView extends JFrame{
         tabbedPane= new JTabbedPane(JTabbedPane.NORTH);//卡在左侧 
         tabbedPane.validate();
         add(tabbedPane,BorderLayout.CENTER); 
+        setTitle("付总万岁");
         setBounds(100,100,1280,760);//调整总框图分辨率和初始位置
+        setIconImage(new ImageIcon("软件发布/图像管理/二刺猿.jpg").getImage());
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
     }
